b5214b1b2d3cba12e6203c7d7d83db2f6dc468e2
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2023-10-24 20:15:41 +0300

    FirstTask

---
+++ src/FirstTask.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class FirstTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lab 2:");
+       System.out.println("Input the amount of hryvnia:");
+       long hryvnia1= scanner.nextLong();
+        System.out.println("Input the amount of coins:");
+        byte coint1= scanner.nextByte();
+        Money money1=new Money(hryvnia1, coint1);
+        System.out.println("Input the amount of hryvnia:");
+        long hryvnia2= scanner.nextLong();
+        System.out.println("Input the amount of coins:");
+        byte coint2= scanner.nextByte();
+        Money money2=new Money(hryvnia2, coint2);
+        System.out.println("money1: " + money1);
+        System.out.println("money2: " + money2);
+
+        Money sum =money1.Add(money2);
+        System.out.println("Summa="+sum);
+
+        Money difference = money1.subtract(money2);
+        System.out.println("Difference: " + difference);
+
+        System.out.print("Enter a divisor for division: ");
+        long divisor = scanner.nextLong();
+        Money division = money1.divisionValue(divisor);
+        System.out.println("Result of division by " + divisor + ": " + division);
+
+
+            Money divisionMoney = money1.divide(money2);
+            System.out.println("Result of money1 division by money2: " + divisionMoney);
+
+
+        int comparison = money1.compareTo(money2);
+        if (comparison < 0) {
+            System.out.println("Money1 is less than money2");
+        } else if (comparison > 0) {
+            System.out.println("Money1 is greater than money2");
+        } else {
+            System.out.println("Money1 is equal to money2");
+        }
+        scanner.close();
+    }
+}
---
+++ src/Money.java
@@ -0,0 +1,66 @@
+public class Money {
+    private final long hryvnias;
+    private final byte coint;
+
+    public Money(long hryvnias, byte coint) {
+        this.coint = coint;
+        this.hryvnias = hryvnias;
+
+    }
+
+    public Money Add(Money other) {
+        long newHrywna = this.hryvnias + other.hryvnias;
+        int newCoint = this.coint + other.coint;
+        if (newCoint >= 100) {
+            newHrywna += newCoint / 100;
+            newCoint %= 100;
+        }
+        return new Money(newHrywna, (byte) newCoint);
+    }
+
+    public Money subtract(Money other) {
+        long newHrywnia = this.hryvnias - other.hryvnias;
+        int newCoint = Math.abs(this.coint - other.coint);
+
+        return new Money(newHrywnia, (byte) newCoint);
+    }
+
+    public Money divisionValue(long Divisor) {
+        long newHrywnia = this.hryvnias / Divisor;
+        int newCoint = (int) (((this.hryvnias % Divisor) * 100 + this.coint) / Divisor);
+
+        return new Money(newHrywnia, (byte) newCoint);
+    }
+
+    public Money divide(Money other) {
+        long thisCoin = this.hryvnias * 100 + this.coint;
+        long otherCoin = other.hryvnias * 100 + other.coint;
+
+
+        double resultCoin = (double) thisCoin / otherCoin;
+
+        long resultHrywnia = (long) resultCoin;
+        byte resultCentsByte = (byte) ((resultCoin - resultHrywnia) * 100);
+
+        return new Money(resultHrywnia, resultCentsByte);
+    }
+
+
+    public int compareTo(Money other) {
+        if (this.hryvnias < other.hryvnias) {
+            return -1;
+        } else if (this.hryvnias > other.hryvnias) {
+            return 1;
+        }
+
+            /*0 if ‘a is equal to ‘b,
+            a positive value ‘a’ is greater than ‘b’,
+            a negative value ‘b’ is greater than ‘a’*/
+        return Byte.compare(this.coint, other.coint);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d,%02d грн.", hryvnias, coint);
+    }
+}
